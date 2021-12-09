d172d9a64fa363e24905d5dfc0f5eba088a86fb7
nico <nicomanzonelli@gmail.com>
2021-12-09 01:12:48 -0500

    add tests for multi-variate optimization

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -7,8 +7,8 @@ class TestOptimizers():
     @classmethod
     def setup_class(TestOptimizers):
         """Set up variables to use in many test cases."""
-        f1 = lambda v: v[0]**2 
-        f2 = lambda v: v[0]**2 + v[1]**3 + 2
+        f1 = lambda v: v[0]**2 #univariate case
+        f2 = lambda v: v[0]**2 + v[1]**2 #multi-variate case
         return [f1, f2]
     
     def test_one_a(self):
@@ -18,6 +18,14 @@ class TestOptimizers():
         assert r1 == pytest.approx(0, .001)
         assert r2 == pytest.approx(0, .001)
         
+    def test_one_b(self):
+        """test gradient decsent for function 2"""
+        f1, f2 = self.setup_class()
+        r1, r2 = gradient_decent(f2, [1,1])
+        assert r1 == pytest.approx(0, .001)
+        assert r2[0] == pytest.approx(0, .001)
+        assert r2[1] == pytest.approx(0, .001)
+        
     def test_two_a(self):
         """test momentum for function 1"""
         f1, f2 = self.setup_class()
@@ -25,9 +33,26 @@ class TestOptimizers():
         assert r1 == pytest.approx(0, .001)
         assert r2 == pytest.approx(0, .001)
         
+    def test_two_b(self):
+        """test momentum for function 2"""
+        f1, f2 = self.setup_class()
+        r1, r2 = momentum_gd(f2, [1,1])
+        assert r1 == pytest.approx(0, .001)
+        assert r2[0] == pytest.approx(0, .001)
+        assert r2[1] == pytest.approx(0, .001)
+        
     def test_three_a(self):
-        """test momentum for function 1"""
+        """test adagrad for function 1"""
         f1, f2 = self.setup_class()
         r1, r2 = adagrad(f1, [1])
         assert r1 == pytest.approx(0, .001)
         assert r2 == pytest.approx(0, .001)
+
+    def test_three_b(self):
+        """test adagrad for function 2"""
+        f1, f2 = self.setup_class()
+        r1, r2 = adagrad(f2, [1,1])
+        assert r1 == pytest.approx(0, .001)
+        assert r2[0] == pytest.approx(0, .001)
+        assert r2[1] == pytest.approx(0, .001)
+
