0b69c833a5e01df596471166e0a26e96bdaddfa9
Nico Manzonelli <47954096+nicomanzonelli@users.noreply.github.com>
2021-12-10 11:32:48 -0500

    edit setup files for PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
-import setuptools
+from distutils.core import setup
 
-setuptools.setup(name='zapnAD',
-      version='1.0',
-      packages= setuptools.find_packages(),
+setup(
+  name = 'zapnAD',         
+  packages = ['zapnAD'],
+  version = '0.1',
+  license='GPL-3.0'
+  description = 'A package for automatic differentiation', 
+  url = 'https://github.com/cs107-zapn/cs107-FinalProject/tree/main,
+  download_url = 'https://github.com/cs107-zapn/cs107-FinalProject/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
+  keywords = ['optimization', 'autodifferntiation'], 
+  install_requires=[         
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',     
+    'License :: OSI Approved :: GPL-3.0',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
