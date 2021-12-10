cdd68d9fb5d23ff17f5500eb51b7522dcad51442
Nico Manzonelli <47954096+nicomanzonelli@users.noreply.github.com>
2021-12-10 15:18:36 -0500

    update setup for final release

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
   license='GPL-3.0',
   description = 'A package for automatic differentiation', 
   url = 'https://github.com/cs107-zapn/cs107-FinalProject',
-  download_url = 'https://github.com/cs107-zapn/cs107-FinalProject/archive/refs/tags/v.03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/cs107-zapn/cs107-FinalProject/archive/refs/tags/v1.0.tar.gz',
   keywords = ['optimization', 'autodifferntiation'], 
   install_requires=[         
           'numpy'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
